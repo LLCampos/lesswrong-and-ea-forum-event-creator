a6d24a8c188dd9ab104b718e25a5f285418f8817
LLCampos <luis.filipe.lcampos@gmail.com>
2022-05-10 18:51:32 +0100

    wip

--- event-creator.py
+++ event-creator.py
@@ -3,12 +3,13 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from configparser import ConfigParser
+import datetime
 
 config = ConfigParser()
 config.read("conf.ini")
 
 
-def create_ea_forum_event(month, year):
+def create_ea_forum_event(meetup_date):
     driver = webdriver.Chrome()
     driver.get("https://forum.effectivealtruism.org/")
 
@@ -22,7 +23,7 @@ def create_ea_forum_event(month, year):
 
     sleep(1)
 
-    title = f"ACX/EA Lisbon {month} {year} Meetup"
+    title = f"ACX/EA Lisbon {meetup_date.strftime('%B')} {meetup_date.year} Meetup"
     driver.find_element(By.XPATH, "//*[@placeholder='Title']").send_keys(title)
 
     description = ("Exact location: https://plus.codes/8CCGPRPW+WF\n"
@@ -32,8 +33,18 @@ def create_ea_forum_event(month, year):
     driver.find_element(By.XPATH, "//label[text()='Event Format']/following-sibling::div").click()
     driver.find_element(By.XPATH, "//li[text()='Social']").click()
 
-    sleep(22345354)
+    formatted_date = meetup_date.strftime("%m/%d/%Y")
+    start_time = driver.find_element(By.NAME, "startTime")
+    start_time.click()
+    start_time.send_keys(f"{formatted_date} 15:00")
+
+    end_time = driver.find_element(By.NAME, "endTime")
+    end_time.click()
+    end_time.send_keys(f"{formatted_date} 18:00")
+
+    sleep(123)
 
 
 if __name__ == "__main__":
-    create_ea_forum_event(2022, "June")
+    meetup_date = datetime.datetime(2022, 6, 22)
+    create_ea_forum_event(meetup_date)
